a26c297a231701c93eb1c44eceeb6bae9c7848d9
newu2012 <newu2011@gmail.com>
2023-04-12 00:12:35 +0500

    Add new editorView and code for fetching tasks

--- BornToCode_Frontend/main.js
+++ BornToCode_Frontend/main.js
@@ -1,27 +1,56 @@
 let url = import.meta.env;
 import {EditorView, basicSetup} from "codemirror";
+import {json} from "@codemirror/lang-json";
 import {html} from "@codemirror/lang-html";
 
 url = window.location.origin;
 console.log(import.meta.url);
 console.log(url);
 
-let editor = new EditorView({
-    extensions: [basicSetup, html()],
-    parent: document.querySelector("#textEditor"),
+let jsonEditor = new EditorView({
+    extensions: [basicSetup, json()],
+    parent: document.querySelector("#jsonEditor"),
 });
+
+let htmlEditor = new EditorView({
+    extensions: [basicSetup, html()],
+    parent: document.querySelector("#htmlEditor"),
+});
+
+export async function getTasks() {
+    const response = await fetch(`${url}/Task/GetTasks`);
+    const data = await response.text();
+
+    jsonEditor.dispatch({
+        changes: {from: 0, to: jsonEditor.state.doc.toString().length, insert: data}
+    });
+}
+
+export async function postNewTask() {
+    let jsonEditorValue = jsonEditor.state.doc.toString();
+    let json = jsonEditorValue;
+
+    await fetch(`${url}/Task/PostNewTask`,
+        {
+            method: "POST",
+            body: json,
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+}
 
 export async function getLastCode() {
     const response = await fetch(`${url}/Code/Get`);
     const data = await response.json();
 
-    editor.dispatch({
-        changes: {from: 0, to: editor.state.doc.toString().length, insert: data}
+    htmlEditor.dispatch({
+        changes: {from: 0, to: htmlEditor.state.doc.toString().length, insert: data}
     });
 }
 
 export async function postNewCode() {
-    let htmlEditorValue = editor.state.doc.toString();
+    let htmlEditorValue = htmlEditor.state.doc.toString();
     let json = JSON.stringify({html: htmlEditorValue});
 
     await fetch(`${url}/Code/Post`,
@@ -49,6 +78,8 @@ export async function fetchWeatherFromServer() {
     console.log({forecasts: data, loading: false});
 }
 
+document.getElementById('getTasks').addEventListener('click', getTasks);
+document.getElementById('postNewTask').addEventListener('click', postNewTask);
 document.getElementById('getLastCode').addEventListener('click', getLastCode);
 document.getElementById('postNewCode').addEventListener('click', postNewCode);
 document.getElementById('fetchWeather').addEventListener('click', fetchWeatherFromServer);
