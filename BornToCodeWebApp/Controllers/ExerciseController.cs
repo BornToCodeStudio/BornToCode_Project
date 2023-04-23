5d0a68be3ad67297c5c34ab6acc93b9517424e8a
newu2012 <newu2011@gmail.com>
2023-04-23 23:59:41 +0500

    Update repositories for swagger documentation

--- BornToCodeWebApp/Controllers/ExerciseController.cs
+++ BornToCodeWebApp/Controllers/ExerciseController.cs
@@ -1,11 +1,14 @@
 using DataContext.Exercises;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace BornToCodeWebApp.Controllers;
 
 [ApiController]
-[Route("[controller]/[action]")]
+[Route("/api/[controller]")]
+[Produces("application/json")]
 public class ExerciseController : ControllerBase
 {
     private readonly IExercisesRepository _exercisesRepository;
@@ -18,18 +21,32 @@ public class ExerciseController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<List<ExerciseModel>> GetExercises()
+    [SwaggerResponse(200, "exerciseModel", typeof(List<ExerciseModel>))]
+    [SwaggerResponse(204, "no exercises")]
+    public async Task<Results<Ok<List<ExerciseModel>>, NoContent>> GetExercises()
     {
         var exerciseModels = await _exercisesRepository.GetAll().ToListAsync();
         _logger.LogInformation("Got {ExerciseModelsCount} ExerciseModels", exerciseModels.Count);
-        return exerciseModels;
+
+        if (exerciseModels.Count > 0)
+            return TypedResults.Ok(exerciseModels);
+
+        return TypedResults.NoContent();
     }
 
     [HttpPost]
-    public async Task<ActionResult> PostNewExercise(CreateExerciseModel newExercise)
+    [SwaggerResponse(200, "exerciseModel", typeof(ExerciseModel))]
+    [SwaggerResponse(400, type: typeof(CreateExerciseModel))]
+    public async Task<Results<Created<ExerciseModel>, BadRequest>> PostNewExercise(CreateExerciseModel newExercise)
     {
         var exerciseModel = await _exercisesRepository.AddNewExerciseAsync(newExercise);
+        if (exerciseModel == null)
+        {
+            _logger.LogInformation("Error at creating new exercise from AuthorId \"{AuthorId}\"", newExercise.AuthorId);
+            return TypedResults.BadRequest();
+        }
+
         _logger.LogInformation("Created new exercise with title \"{ExerciseModel}\"!", exerciseModel.Title);
-        return Created($"{exerciseModel.Id}", exerciseModel);
+        return TypedResults.Created($"{exerciseModel.Id}", exerciseModel);
     }
 }
