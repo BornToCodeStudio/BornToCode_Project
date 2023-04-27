d183d5309ca1f64480154af63782e254ca63d25d
ConquestSucks <exenterprise1337@gmail.com>
2023-04-27 22:21:44 +0300

    Новое название проекта

---
+++ borntocode_frontend/src/views/home-view.vue
@@ -0,0 +1,33 @@
+<template>
+    <main>
+      <div class="main__upper">
+        <input type="text" id="search__bar" placeholder="Search..">
+        <button id="create__task">Create Task</button>
+      </div>
+      <hr>
+    </main>
+</template>
+
+<script>
+export default {
+  name: "home-view"
+};
+</script>
+
+<style lang="scss" scoped>
+    main {
+      
+
+      .main__upper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        #search__bar {
+          width: 1100px;
+          height: 45px;
+          border-radius: 15px;
+        }
+      }
+    }
+</style>
