c283c4314e2bdd86c01c7ec2714f3e6739e479a2
ConquestSucks <exenterprise1337@gmail.com>
2023-04-29 21:28:25 +0500

    fix buttons(((

--- borntocode_frontend/src/components/nav-bar.vue
+++ borntocode_frontend/src/components/nav-bar.vue
@@ -36,8 +36,6 @@ export default {
 
 <style lang="scss" scoped>
     .nav__bar {
-        user-select: none;
-        pointer-events: none;
         display: flex;
         flex-direction: row;
         gap: 20px;
